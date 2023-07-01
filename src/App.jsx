bd7e067c812f71924e79ac56c1bc0246d5c90d3d
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-07-01 16:57:23 +0530

    removed react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import Home from './pages/Home'
-import { BrowserRouter as Router} from 'react-router-dom';
 
 import './App.css'
 
 function App() {
 
   return (
-    <Router>
-      <Home/> 
-    </Router>
+    <Home/>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
 
 import './Home.scss'
 
