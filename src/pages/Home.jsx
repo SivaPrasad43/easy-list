ed5cbf600a1555698b8df527eee540850c707eb7
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-07-01 08:59:52 +0530

    size-filter-added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
 
@@ -8,115 +9,202 @@ import VideosData from './data/videos.json'
 
 
 const Home = () => {
-    return (
-        <div className="home_container">
-            <div className="list_box">
-                <div className="list_title">List</div>
-                <div className="list_container">
-                    <ul>
-                        <ItemContainer 
-                            title={PhotosData.title} 
-                            data={PhotosData.data}
-                            icon={PhotosData.icon}
-                            bgColor={PhotosData.bgColor}
-                        />
-                        <ItemContainer 
-                            title={VideosData.title} 
-                            data={VideosData.data}
-                            icon={VideosData.icon}
-                            bgColor={VideosData.bgColor}
-                        />
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="home_container">
+      <div className="list_box">
+        <div className="list_title">List</div>
+        <div className="list_container">
+          <ul>
+            <ItemContainer
+              title={PhotosData.title}
+              data={PhotosData.data}
+              icon={PhotosData.icon}
+              bgColor={PhotosData.bgColor}
+            />
+            <ItemContainer
+              title={VideosData.title}
+              data={VideosData.data}
+              icon={VideosData.icon}
+              bgColor={VideosData.bgColor}
+            />
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-const ItemContainer = ({title,data,icon,bgColor}) => {
-    const [expanded, setExpanded] = useState(false);
-    const [categoryItems, setCategoryItems] = useState([]);
-    const [activeItem,setActiveItem] = useState("All");
-    const [cardData,setCardData] = useState([])
-  
-    const handleClick = () => {
-      setExpanded(!expanded);
-    };
+const ItemContainer = ({ title, data, icon, bgColor }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [categoryItems, setCategoryItems] = useState([]);
+  const [activeItem, setActiveItem] = useState("All");
+  const [cardData, setCardData] = useState([])
+  const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        console.log(data);
-        setCardData(data)
-        const uniqueFormats = [...new Set(data?.map(item => item.format))];
-        setCategoryItems(['All', ...uniqueFormats]);
-    },[data]);
+  const [minSize, setMinSize] = useState(1)
+  const [maxSize, setMaxSize] = useState(10000)
 
-    useEffect(()=>{
-        if(activeItem === "All"){
-            setCardData(data)
-        }else{
-            const filteredData = data?.filter(item=>item.format === activeItem)
-            setCardData(filteredData)
-        }
-    },[activeItem])
-  
-    return (
-      <li>
-        <div className="item_container" onClick={handleClick}>
-          <div className="item_title">
-            <i className="fi fi-rr-folder"></i>
-            <p>{title}</p>
-          </div>
-          <i className={`fi fi-ss-angle-small-${expanded ? "up" : "down"}`}></i>
+  const sizeArray = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+
+  useEffect(() => {
+    console.log(data);
+    setCardData(data)
+    const uniqueFormats = [...new Set(data?.map(item => item.format))];
+    setCategoryItems(['All', ...uniqueFormats]);
+    setLoading(false)
+  }, [expanded]);
+
+  function filterDatas(){
+    if (activeItem === "All") {
+      setCardData(data?.filter(item => item.size >= minSize && item.size <= maxSize))
+    } else {
+      const filteredData = data?.filter(item => item.format === activeItem && item.size >= minSize && item.size <= maxSize)
+      setCardData(filteredData)
+    }
+  }
+
+  useEffect(() => {
+    filterDatas()
+  }, [activeItem])
+
+  useEffect(() => {
+    const filteredData = cardData?.filter(item => item.size >= minSize && item.size <= maxSize)
+    setCardData(filteredData)
+    filterDatas()
+  }, [minSize, maxSize])
+
+  function formatFileSize(sizeInKB) {
+    if (sizeInKB < 1024) {
+      return sizeInKB + " KB";
+    } else if (sizeInKB < 1048576) {
+      const sizeInMB = (sizeInKB / 1024).toFixed(2);
+      return sizeInMB + " MB";
+    } else {
+      const sizeInGB = (sizeInKB / 1048576).toFixed(2);
+      return sizeInGB + " GB";
+    }
+  }
+
+  function handleMinChange(e) {
+    setMinSize(e.target.value)
+  }
+
+  function handleMaxChange(e) {
+    setMaxSize(e.target.value)
+  }
+
+  return (
+    <li>
+      <div className="item_container" onClick={handleClick}>
+        <div className="item_title">
+          <i className="fi fi-rr-folder"></i>
+          <p>{title}</p>
         </div>
-        {expanded && (
-          <div className="item_expand">
-            <div className="category_container">
-              <ul>
-                {
-                    categoryItems.map((item,index)=>(
-                        <li key={index}>
-                        <div 
-                            className={`category_item ${activeItem === item ? "category_active" : ""}`}
-                            onClick={()=>{
-                                setActiveItem(item)
-                            }}
-                        >
-                          <div className="category_title">{item}</div>
-                        </div>
-                      </li>
+        <i className={`fi fi-ss-angle-small-${expanded ? "up" : "down"}`}></i>
+      </div>
+      {expanded && (
+        <div className="item_expand">
+          <div className="category_container">
+            <ul>
+              {
+                categoryItems.map((item, index) => (
+                  <CategoryItem
+                    key={index}
+                    item={item}
+                    activeItem={activeItem}
+                    setActiveItem={setActiveItem}
+                  />
+                ))
+              }
+            </ul>
+            <div className="size_container">
+              <p>Size:</p>
+              <div className="size_box">
+                <div className='size_title'>Min</div>
+                <select name="min" id="min" value={minSize} onChange={handleMinChange}>
+                  {
+                    sizeArray.map((item, index) => (
+                      <option key={index} value={item}>{formatFileSize(item)}</option>
                     ))
-                }
-              </ul>
-            </div>
-            <div className="card_container">
-                {
-                    cardData.map((item,index)=>(
-                        <Cards
-                            key={index}
-                            size={item.size}
-                            url={item.url}
-                            img={icon}
-                            bgColor={bgColor}
-                        />
+                  }
+                </select>
+              </div>
+              <div className="size_box">
+                <div className='size_title'>Max</div>
+                <select name="max" id="max" value={maxSize} onChange={handleMaxChange}>
+                  {
+                    sizeArray.map((item, index) => (
+                      <option key={index} value={item}>{formatFileSize(item)}</option>
                     ))
-                }
+                  }
+                </select>
+              </div>
             </div>
           </div>
-        )}
-      </li>
-    );
+          {
+            loading ? (
+              <p>Loading..</p>
+            ) : (
+              <div className="card_container">
+                {
+                  cardData.map((item, index) => (
+                    <Cards
+                      key={index}
+                      size={item.size}
+                      format={item.format}
+                      url={item.url}
+                      img={icon}
+                      bgColor={bgColor}
+                    />
+                  ))
+                }
+                {
+                  cardData.length === 0 && <p>File not exist !</p>
+                }
+              </div>
+            )
+          }
+        </div>
+      )}
+    </li>
+  );
 };
 
-const Cards = ({size,img,bgColor})=> {
-    return(
-        <div className="item_expand_card">
-        <div className="icon_container" style={{backgroundColor:bgColor}}>
-        <i className={`fi ${img}`}></i>
-        </div>
-        <div className="item_size">{size}</div>
-        <a href='#' className="download_btn">Download</a>
+const CategoryItem = ({ item, activeItem, setActiveItem }) => {
+  return (
+    <li>
+      <div
+        className={`category_item ${activeItem === item ? "category_active" : ""}`}
+        onClick={() => {
+          setActiveItem(item)
+        }}
+      >
+        <div className="category_title">{item}</div>
       </div>
-    )
+    </li>
+  )
+}
+
+const Cards = ({ size, format, url, img, bgColor }) => {
+  return (
+    <div className="item_expand_card">
+      <div className="icon_container" style={{ backgroundColor: bgColor }}>
+        <i className={`fi ${img}`}></i>
+      </div>
+      <div className="item_details">
+        <div className="item_size">{size}</div>
+        <i className="fi fi-ss-bullet"></i>
+        <div className="item_format">{format}</div>
+      </div>
+      <a href={url} className="download_btn">
+        <i className="fi fi-rr-download"></i>
+        Download</a>
+    </div>
+  )
 }
 
 export default Home
